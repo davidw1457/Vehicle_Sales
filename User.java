bdd7f08275d0e6c45105adc424716d31011f73a2
davidw1457 <89819150+davidw1457@users.noreply.github.com>
2021-11-15 19:30:54 -0600

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+public class User {
+    private String username;
+    private boolean admin;
+
+    public User(String username, boolean admin) {
+        this.username = username;
+        this.admin = admin;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
